cc7e3684c367aea4928aaf89bb7f1c30198298e6
Nick <nik_usov.1998@mail.ru>
2026-06-26 16:10:09 +0500

    Update Makefile LDFLAGS Update raylib header and lib file Add basic window creation in main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
 #include "include/raylib.h"
 
 int main(int argc, char* argv[])
 {
-  printf("Hello, World!\n");
+  const int screenWidth = 640;
+  const int screenHeight = 480;
+  InitWindow(screenWidth, screenHeight, "Hello, World!");
+  SetTargetFPS(60);
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Test Sample", 320, 240, 24, LIGHTGRAY);
+    EndDrawing();
+  }
+  CloseWindow();
+
   return 0;
 }
 
